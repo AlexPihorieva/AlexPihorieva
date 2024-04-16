9117cbab8f1a40a0608789c0386e09c7482d4b26
Oleksandra Pihorieva <pigorevas@gmail.com>
2024-04-16 21:13:30 +0300

    Homework 34

---
+++ exercises/lecture-34/gallery/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(props){
+    return (
+        <footer>
+        <p>Author: Oleksandra Pihorieva</p>
+        <p><a href="mailto:hege@example.com">hege@example.com</a></p>
+      </footer>
+    )
+}
+
+export default Footer;
---
+++ exercises/lecture-34/gallery/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar(props){
+    return (
+    <nav>
+        <a href="/html/">HTML</a> |
+        <a href="/css/">CSS</a> |
+        <a href="/js/">JavaScript</a> |
+        <a href="/react/">React</a>
+    </nav>
+    )
+}
+
+export default Navbar;
